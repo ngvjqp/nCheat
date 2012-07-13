553d1280c63ceaf2c38c345ce639296ac71db0fa
unknown <Nelito@Nelito-desk.(none)>
2012-07-13 01:31:51 -0300

    Inicio

---
+++ nCheat/src/com/ngvj10/ncheat/MainActivity.java
@@ -0,0 +1,56 @@
+package com.ngvj10.ncheat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	Button btMat;
+	Button btQmq;
+	Button btFsc;
+	Button btOutros;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		btMat = (Button) findViewById(R.id.btMat);
+		btQmq = (Button) findViewById(R.id.btQmq);
+		btFsc = (Button) findViewById(R.id.btFsc);
+		btOutros = (Button) findViewById(R.id.btOutros);
+
+		btMat.setOnClickListener((android.view.View.OnClickListener) this);
+		btQmq.setOnClickListener((android.view.View.OnClickListener) this);
+		btFsc.setOnClickListener((android.view.View.OnClickListener) this);
+		btOutros.setOnClickListener((android.view.View.OnClickListener) this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (btMat.getId() == ((Button) v).getId()) {
+			startActivity(new Intent(this, matematica.class));
+		}
+		if (btQmq.getId() == ((Button) v).getId()) {
+
+		}
+		if (btFsc.getId() == ((Button) v).getId()) {
+
+		}
+		if (btOutros.getId() == ((Button) v).getId()) {
+
+		}
+
+	}
+}
